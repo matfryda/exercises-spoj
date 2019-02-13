793b770b11ccc4aec82f11e6d3251e88609a7311
MateuszFryda <mateuszfryda@gmail.com>
2019-02-13 14:08:30 +0100

    Now it works well :)

--- src/main/java/obzartuchy/Main.java
+++ src/main/java/obzartuchy/Main.java
@@ -7,33 +7,30 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        // System.out.println("Podaj liczbę testów");
-        int iloscZestawowTestowych = scanner.nextInt();
-        final int sekundyWDobie = 86400;
+         System.out.println("How many tests?");
+        int numberOfTests = scanner.nextInt();
+        final int secondsInADay = 86400;
 
 
-        for (int i = 0; i < iloscZestawowTestowych; i++) {
-            // System.out.println("Podaj liczbę uczestników");
+        for (int i = 0; i < numberOfTests; i++) {
+             System.out.println("enter the number of participants");
             int n = scanner.nextInt();
             int[] tab = new int[n];
-            int czasJedzeniaWSekundach;
-            System.out.println("Podaj liczbę ciastek w jednym pudełku");
+            int eatingTimeInSeconds;
+            System.out.println("Enter the number of cookies in one box");
             int m = scanner.nextInt();
             for (int j = 0; j < n; j++) {
-                //System.out.println("podaj czas jedzenia "+(j+1)+" zawodnika");
-                tab[j] = czasJedzeniaWSekundach = scanner.nextInt();
-                // System.out.println(tab[j]);
+                System.out.println("enter the player's eating time " + (j + 1));
+                tab[j] = eatingTimeInSeconds = scanner.nextInt();
+                 System.out.println(tab[j]);
 
             }
             int[] tab2 = new int[n];
             for (int j = 0; j < n; j++) {
-                int iloscZjedzonychCiastek = sekundyWDobie / tab[j];
-                tab2[j] = iloscZjedzonychCiastek;
-                // System.out.println(tab2[j]);
-
-
-                // System.out.println(iloscZjedzonychCiastek);
+                int theAmountOfCookiesEaten = secondsInADay / tab[j];
+                tab2[j] = theAmountOfCookiesEaten;
             }
+
             int suma = 0;
             for (int j = 0; j < tab2.length; j++) {
                 suma += tab2[j];
